8672727ff4e9bfbd6315b45c7ddb148ee35734d4
AlzDevs <121944697+AlzDevs@users.noreply.github.com>
2026-04-03 02:56:11 +0700

    Update install-windowa.sh

--- install-windowa.sh
+++ install-windowa.sh
@@ -39,7 +39,7 @@ show_banner() {
     echo -e "${BLUE}"
     echo "╔══════════════════════════════════════════════════════════════╗"
     echo "║           AUTO INSTALLER WINDOWS SERVER VPS DO               ║"
-    echo "║                    by AlzDev                     ║"
+    echo "║                    by AlzDev                                 ║"
     echo "╚══════════════════════════════════════════════════════════════╝"
     echo -e "${NC}"
 }
